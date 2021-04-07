19f709649bc6fb87477858478f97397fb4e463e3
fx67ll <2717745014@qq.com>
2021-04-07 16:32:17 +0800

    use local database

--- express-hello/mongodb/index.js
+++ express-hello/mongodb/index.js
@@ -1,7 +1,7 @@
 // 引入环境变量插件
-require('dotenv').config({
-	path: '../.env'
-});
+// require('dotenv').config({
+// 	path: '../.env'
+// });
 
 // mongoose插件帮助操作数据库
 var mongoose = require('mongoose')
@@ -9,7 +9,8 @@ var mongoose = require('mongoose')
 var InitiateMongoServer = async () => {
 
 	//连接MongoDB数据库
-	mongoose.connect(process.env.MONGODB_URL)
+	// mongoose.connect(process.env.MONGODB_URL)
+	mongoose.connect('mongodb://127.0.0.1:27017/wxapp')
 
 	//连接成功
 	mongoose.connection.on("connected", function() {
