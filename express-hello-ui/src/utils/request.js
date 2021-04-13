8717034117fa617f514f73426e23e5a6ae304386
fx67ll <2717745014@qq.com>
2021-04-13 17:11:41 +0800

    add token check

--- express-hello-ui/src/utils/request.js
+++ express-hello-ui/src/utils/request.js
@@ -4,7 +4,7 @@ import {
 	MessageBox,
 	Message
 } from 'element-ui'
-// import { getToken } from '@/utils/auth'
+import { getToken } from '@/utils/auth'
 import errorCode from '@/utils/errorCode'
 
 axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8'
@@ -22,6 +22,10 @@ service.interceptors.request.use(config => {
 	// if (getToken() && !isToken) {
 	//   config.headers['Authorization'] = 'Bearer ' + getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
 	// }
+	const isToken = (config.headers || {}).isToken === false
+	if (getToken() && !isToken) {
+	  config.headers['token'] = getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
+	}
 	// get请求映射params参数
 	if (config.method === 'get' && config.params) {
 		let url = config.url + '?';
